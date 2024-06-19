4bedb32fb50b53941453c3a2d74394c41725ec2c
hiddenblue <62304226+hiddenblue@users.noreply.github.com>
2024-06-19 11:28:17 +0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,85 @@
+﻿using static System.Console;
+using CalculatorLibrary;
+
+namespace CalculatorProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool endApp = false;
+
+            WriteLine("Console Calculator in C#\r");
+            WriteLine("--------------------------------\n");
+
+            // 初始化化一个计算器的实例
+            Calculator calculator = new Calculator();
+
+
+            while (!endApp)
+            {
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+                
+                WriteLine("Type a number, and then press Enter");
+                numInput1 = ReadLine();
+
+                double cleanNum1 = 0;
+                while(!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Write("This is not valid input. Please enter an integer value: ");
+                    numInput2 = ReadLine();
+                }
+
+                WriteLine("Type another number, and then press Enter: ");
+                numInput2 = ReadLine();
+
+
+                double cleanNum2 = 0;
+
+                while(!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Write("This is not valid input. Please enter an integer value: ");
+                    numInput2 = ReadLine();
+                }
+
+                WriteLine("Choose an option from the following list:");
+                WriteLine("\ta - Add");
+                WriteLine("\ts - Subtract");
+                WriteLine("\tm - Multiply");
+                WriteLine("\td - Divide");
+                Write("Your option? ");
+
+                string op = ReadLine();
+
+                try
+                {
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+
+                    if (double.IsNaN(result))
+                    {
+                        WriteLine("This operation will result in a mathmatical error.\n");
+                    }
+                    else
+                        WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    WriteLine("oh no! An exception occured trying to do the math.\n - Details: " + e.Message);
+                }
+
+                WriteLine("-----------------------------------------------\n");
+
+                WriteLine("Press 'n' and Enter to close the app, or press another key and Enter to continue: ");
+                if (ReadLine() != "n")
+                {
+                    endApp = true;
+                }
+            }
+
+            return;
+        }
+    }
+}
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+
+    // 从原来的纯粹的静态方法，改成需要实例化的形式
+    // 初始化的时候还会自动开始记录日志
+    public class Calculator
+    {
+        // constructor method
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculator.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            Trace.AutoFlush = true;
+            Trace.WriteLine("Starting Calculator Long");
+            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+
+        }
+
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
